1d006f6c27fc6fc63412b0c7efa78a2716e9e9c8
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-05-20 23:55:53 +0300

    refactor: make properties and methods private, improve encapsulation

--- countdown-timer.js
+++ countdown-timer.js
@@ -6,42 +6,47 @@ export class CountdownTimer extends HTMLElement {
         customElements.define(tagName, this);
     }
 
+    #untilDate = null;
+    #prefix = 'countdown';
+    #timerInterval = null;
+
     constructor() {
         super();
-        this.timerInterval = null;
-        this.untilDate = null;
+
+        // Derive prefix by removing the '-timer' suffix from the element's tag name
+        this.#prefix = this.tagName.toLowerCase().slice(0, -6);
     }
 
     connectedCallback() {
         const until = this.getAttribute('until');
         if (until) {
-            this.untilDate = new Date(until);
-            this.startTimer();
+            this.#untilDate = new Date(until);
+            this.#startTimer();
         } else {
             console.error('countdown-timer requires an "until" attribute.');
         }
     }
 
     disconnectedCallback() {
-        this.stopTimer();
+        this.#stopTimer();
     }
 
-    startTimer() {
-        this.updateTimer();
-        this.stopTimer();
-        this.timerInterval = setInterval(() => this.updateTimer(), 1000);
+    #startTimer() {
+        this.#updateTimer();
+        this.#stopTimer();
+        this.#timerInterval = setInterval(() => this.#updateTimer(), 1000);
     }
 
-    stopTimer() {
-        if (this.timerInterval) {
-            clearInterval(this.timerInterval);
-            this.timerInterval = null;
+    #stopTimer() {
+        if (this.#timerInterval) {
+            clearInterval(this.#timerInterval);
+            this.#timerInterval = null;
         }
     }
 
-    updateTimer() {
+    #updateTimer() {
         const now = new Date();
-        const units = this.getUnits();
+        const units = this.#getUnits();
 
         let timeRemaining = {
             years: 0,
@@ -53,10 +58,10 @@ export class CountdownTimer extends HTMLElement {
             seconds: 0,
         };
 
-        if (now >= this.untilDate) {
-            this.stopTimer();
+        if (now >= this.#untilDate) {
+            this.#stopTimer();
         } else {
-            timeRemaining = formatInterval(now, this.untilDate, units);
+            timeRemaining = formatInterval(now, this.#untilDate, units);
         }
 
         for (const unit of Object.keys(units)) {
@@ -66,18 +71,15 @@ export class CountdownTimer extends HTMLElement {
         }
     }
 
-    getUnits() {
-        // Derive prefix by removing the '-timer' suffix from the element's tag name
-        const prefix = this.tagName.toLowerCase().slice(0, -6);
-
+    #getUnits() {
         return {
-            years: this.querySelector(`${prefix}-years`),
-            months: this.querySelector(`${prefix}-months`),
-            weeks: this.querySelector(`${prefix}-weeks`),
-            days: this.querySelector(`${prefix}-days`),
-            hours: this.querySelector(`${prefix}-hours`),
-            minutes: this.querySelector(`${prefix}-minutes`),
-            seconds: this.querySelector(`${prefix}-seconds`),
+            years: this.querySelector(`${this.#prefix}-years`),
+            months: this.querySelector(`${this.#prefix}-months`),
+            weeks: this.querySelector(`${this.#prefix}-weeks`),
+            days: this.querySelector(`${this.#prefix}-days`),
+            hours: this.querySelector(`${this.#prefix}-hours`),
+            minutes: this.querySelector(`${this.#prefix}-minutes`),
+            seconds: this.querySelector(`${this.#prefix}-seconds`),
         };
     }
 }
